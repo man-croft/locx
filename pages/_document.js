d80fa424570052cdb205dcb73de12921b15c0a7c
manny <praisechinonso21@gmail.com>
2025-09-10 21:24:11 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="application-name" content="EchoEcho" />
+        <meta name="description" content="Break the echo chamber: EchoEcho finds counter-narratives on Farcaster." />
+        <meta property="og:title" content="EchoEcho" />
+        <meta property="og:description" content="Find counter-narratives for trending Farcaster topics" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/icon-192.png" />
+        <link rel="icon" href="/icon-192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
