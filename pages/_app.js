890dd3d940bb8a43c5f21b6bc92182d8a5c048a8
manny <praisechinonso21@gmail.com>
2025-09-13 01:41:55 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import { sdk } from "@farcaster/frame-sdk";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    sdk.actions.ready(); // Notify Farcaster that your app is ready
+  }, []);
+
   return <Component {...pageProps} />;
 }
