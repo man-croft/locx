8a0c23c243798292c47187b8e92a8a82ccdb91ca
manny <praisechinonso21@gmail.com>
2025-09-13 11:21:21 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,8 @@ export default function Document() {
         <meta property="og:title" content="EchoEcho" />
         <meta property="og:description" content="Find counter-narratives for trending Farcaster topics" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/icon-192.png" />
+        <meta property="og:image" content="https://echoechos.vercel.app/icon-192.png" />
+        <link rel="icon" href="https://echoechos.vercel.app/icon-192.png" />
       </Head>
       <body>
         <Main />
