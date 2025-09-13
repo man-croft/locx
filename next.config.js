319190edddb7192d4871a21b784ea5e7c26edfe5
manny <praisechinonso21@gmail.com>
2025-09-13 02:30:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
+  async redirects() {
     return [
       {
-        source: "/.well-known/farcaster.json",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/json"
-          }
-        ]
-      }
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/0199409c-b991-9a61-b1d8-fef2086f7533',
+        permanent: false, // false → 307
+      },
     ];
-  }
+  },
 };
 
 module.exports = nextConfig;
