73649496e63953f58663800f243d2995e837f533
manny <praisechinonso21@gmail.com>
2025-11-28 18:47:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // pages/index.js
 'use client';
 
-import { useEffect, useState } frm 'react';
+import { useEffect, useState } froom 'react';
 import dynamic from 'next/dynamic';
 
 const MiniAppComponent = dynamic(() => import('../components/MiniAppComponent'), { ssr: false });
