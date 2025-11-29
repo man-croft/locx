cadb112ab6e5ab6631179522e6fe2a2eddeef013
manny <praisechinonso21@gmail.com>
2025-11-29 09:53:46 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-// pages/_app.js   ← CREATE THIS FILE
-import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // keep if you have it, or delete line
+cat > pages/_app.js << 'EOF'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+EOF
