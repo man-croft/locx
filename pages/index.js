0edd5cea7bcef0c28d5f0490e79d2dbd7bace190
manny <praisechinonso21@gmail.com>
2025-11-29 07:53:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // pages/index.js
 'use client';
 
-im { useEffect, useState } from 'react';
+i { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const MiniAppComponent = dynamic(() => import('../components/MiniAppComponent'), { ssr: false });
