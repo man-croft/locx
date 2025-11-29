6b56340ea978971d9faf124890f0600419d388ff
manny <praisechinonso21@gmail.com>
2025-11-29 07:51:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import dynamic from 'next/dy';
+import dynamic from 'next/y';
 
 const MiniAppComponent = dynamic(() => import('../components/MiniAppComponent'), { ssr: false });
 const WagmiConfig = dynamic(() => import('wagmi').then(m => ({ default: m.WagmiConfig })), { ssr: false });
