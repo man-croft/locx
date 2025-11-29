947394e0ca33ef69059420805a93dc3aab11a0a2
manny <praisechinonso21@gmail.com>
2025-11-29 14:44:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import { sdk } from '@farster/miniapp-sdk';
+import { sdk } from '@farcster/miniapp-sdk';
 
 export default function Home() {
   useEffect(() => {
