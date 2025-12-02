e8e8c674ee55b79b621a0f704f82435982002eda
manny <praisechinonso21@gmail.com>
2025-12-02 19:52:40 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { sdk } from '@farcaster/miniapp-sdk';
 
 export default function Home() {
   useEffect(() => {
-    // THIS LINE REMOVES THE SPLASH SCR
+    // THIS LINE REMOVES THE SPLASH SC
     sdk.actions.ready().catch(console.error);
 
     // Optional: log so you know it's alive
