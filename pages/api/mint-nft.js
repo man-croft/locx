9c57d546546b37f51cc7e85a43503b2d7a1c4228
manny <praisechinonso21@gmail.com>
2025-12-07 18:43:16 +0100

    Update mint-nft.js

--- pages/api/mint-nft.js
+++ pages/api/mint-nft.js
@@ -10,7 +10,7 @@ const _publicClient = createPublicClient({
 });
 
 // NFT contract on Base (replace wyour contract)
-const _NFT_CONTRACT = process.env.NFT_CONTRACT || '0xea2a41c02fa86a4901826615f9796e603c6a4491';
+const _NFT_CONTRACT = process.env.NFT_CONTRACT || '0xea22fa86a4901826615f9796e603c6a4491';
 const IPFS_API_KEY = process.env.IPFS_API_KEY
 const IPFS_API_SECRET = process.env.IPFS_SECRET;
 
