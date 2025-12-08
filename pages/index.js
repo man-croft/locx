baa83101280622b5133742233551b09452ae133e
manny <praisechinonso21@gmail.com>
2025-12-08 08:16:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { sdk } from '@farcaster/miniapp-sdk';
 export default function Home() {
   useEffect(() => 
     // THIS LINE REMOVES THE SPLAS
-    sdk.actions.ready().catchconsole.error);
+    sdk.actions.ready().catchcosole.error);
     // Optional: log so you know it's alive
     console.log('ready() called — splash screen will disappear now');
   }, [])
