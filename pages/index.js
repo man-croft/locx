d8421be8e92c156d0823c23b66f9fe79c44c9ef7
manny <praisechinonso21@gmail.com>
2025-12-08 08:20:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     // THIS LINE REMOVES THE SPLAS
     sdk.actions.ready().catch(console.error);
 
-    // Optional: log so 
+    // Optional: log so
     console.log('ready() called — splash screen will disappear now');
   }, []);
 
