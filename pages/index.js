5680e536d6bf58758d24eb6934ff889533a25bc7
manny <praisechinonso21@gmail.com>
2025-12-08 08:22:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { sdk } from '@farcaster/miniapp-sdk';
 
 export default function Home() {
   useEffect(() => {
-    // THIS LINE REMOVES TH
+    // THIS LINE REMOVES T
     sdk.actions.ready().catch(console.error);
 
     // Optional: log s
