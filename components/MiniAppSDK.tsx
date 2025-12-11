17f4f599874715ef47f0bd9faaa8ee43ba7bac97
manny <praisechinonso21@gmail.com>
2025-12-11 21:29:43 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -17,8 +17,7 @@ export default function MiniAppSDK({
     // 2. Optional: get user data safely
     const getUser = async () => {
       try {
-       const context = await kgetLocationContext().catch(() => ({}));
-      const user = await sdk.getUser().catch(() => {}));
+       const context = await kgetLocationContext().catch(() => ({}));      const user = await sdk.getUser().catch(() => {}));
         onUser?.({ ...user, context });
       } catch (e) {
         onUser?.({ error: e.message });
