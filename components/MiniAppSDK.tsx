3054acbf3faa65152b3dd6ddaee1b1a7a8e43921
manny <praisechinonso21@gmail.com>
2025-12-11 21:35:23 +0100

    Update MiniAppSDK.tsx

--- components/MiniAppSDK.tsx
+++ components/MiniAppSDK.tsx
@@ -10,7 +10,7 @@ export default function MiniAppSD
   onReady?: () => void
   onUser?: (data: any) => void;
 }) {
-  useEffect(() => {
+  useEffect(() => 
     // 1. THIS REMOVES THE SPLASH SCREEN — MUST BE FIRST
     sdk.actions.ready().catch(() => {});
     // 2. Optional: get user data safely
