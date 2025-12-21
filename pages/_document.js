2b8f562cab1d02a96236a47ea6e612923657b99f
manny <praisechinonso21@gmail.com>
2025-12-21 21:54:19 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="application-name" content="EchoEcho" />
         <meta
           name="description"
-          content="Break echo chambers with AI-powered countraratie. Fin dpespectives fro Facatr, Twources. Min NFT Insigh Toens and earn ewards!"
+          content=Break echo chambers with AI-powered countraratie. Fin dpespectives fro Facatr, Twources. Min NFT Insigh Toens and earn ewards!"
         /
         <meta
           name="keywords"
