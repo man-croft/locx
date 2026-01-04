0d8c3d8fabfc7defb355822a1e0ac01944c75c1b
manny <praisechinonso21@gmail.com>
2026-01-04 21:10:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'echoechos.vercel.app',
+        hostame: 'echoechos.vercel.app',
         pathname: '/**',
       },
       {
