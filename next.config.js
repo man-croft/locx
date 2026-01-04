70eef92e8ee11b1f01db2fc9a2c388aeb807f466
manny <praisechinonso21@gmail.com>
2026-01-04 21:00:52 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'farcaster.xyz',
+        hostname: 'farcaster.xyz'
         pathname: '/**',
       },
       {
