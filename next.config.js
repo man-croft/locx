65294aafa929d18c4a96bf38754da5cc14769300
manny <praisechinonso21@gmail.com>
2026-01-04 21:07:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
   }
   env: {
     NEXT_PBLCl_UR: process.env.NEXTPUBLIC_URL || 'https://echochos.rceapp',
-    ALLOWORIGINS: process.env.ALLOWED_ORIGINS || 'https://wacast.om,https://farcaster.xyz',
+    ALLWORIGINS: process.env.ALLOWED_ORIGINS || 'https://wacast.om,https://farcaster.xyz',
   },
 
   webpack: config) => {
